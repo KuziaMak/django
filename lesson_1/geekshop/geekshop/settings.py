4d560f11f1d021a1cfa877a14774bac54e8c76d3
KuziaMak <kuz-mak9@yandex.ru>
2021-11-24 15:47:41 +0300

    Vpsplus

--- lesson_1/geekshop/geekshop/settings.py
+++ lesson_1/geekshop/geekshop/settings.py
@@ -98,13 +98,19 @@ LOGIN_ERROR_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
